309bd8eaef156196b75aeb32aefaf0bc36ff07e7
jpv <thevirus4@hotmail.com>
2015-09-15 15:00:09 +0200

    Loaders

--- app/src/main/java/es/jpv/android/examples/loadersexample/MainActivityFragment.java
+++ app/src/main/java/es/jpv/android/examples/loadersexample/MainActivityFragment.java
@@ -24,8 +24,7 @@ import android.widget.Toast;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment
-        implements RVCursorAdapter.OnItemClickListener, RVCursorAdapter.OnItemLongClickListener,
-                    LoaderManager.LoaderCallbacks<Cursor> {
+        implements RVCursorAdapter.OnItemClickListener, RVCursorAdapter.OnItemLongClickListener {
 
     final int LOADER_ID = 1;
     final String[] PROJECTION = new String[] {
@@ -42,8 +41,32 @@ public class MainActivityFragment extends Fragment
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
         RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        final LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(llm);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            /**
+             * Callback method to be invoked when RecyclerView's scroll state changes.
+             *
+             * When the RecyclerView stops scrolling we check the last item that is fully visible.
+             * Should the last visible list item match with the number of items in the adapter,
+             * we have reached the end of the list so we do another query to get more rows
+             *
+             * @param recyclerView The RecyclerView whose scroll state has changed.
+             * @param newState     The updated scroll state.
+             */
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    int lastVisibleItem = llm.findLastCompletelyVisibleItemPosition() + 1;
+                    if (lastVisibleItem >= adapter.getItemCount()) {
+                        Toast.makeText(getActivity(),
+                                "End of list!!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
         adapter = new RVCursorAdapter(getActivity(), null);
         adapter.setOnItemClickListener(this);
         adapter.setOnItemLongClickListener(this);
@@ -67,7 +90,7 @@ public class MainActivityFragment extends Fragment
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        getLoaderManager().initLoader(LOADER_ID, null, this);
+        getLoaderManager().initLoader(LOADER_ID, null, this.new DBLoader());
     }
 
     @Override
@@ -108,80 +131,6 @@ public class MainActivityFragment extends Fragment
     }
 
     /**
-     * Instantiate and return a new Loader for the given ID.
-     *
-     * @param id   The ID whose loader is to be created.
-     * @param args Any arguments supplied by the caller.
-     * @return Return a new Loader instance that is ready to start loading.
-     */
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new DBCursorLoader(
-                getActivity(),
-                DBHelper.getInstance(getActivity()),
-                PROJECTION,
-                null,
-                null,
-                null);
-    }
-
-    /**
-     * Called when a previously created loader has finished its load.  Note
-     * that normally an application is <em>not</em> allowed to commit fragment
-     * transactions while in this call, since it can happen after an
-     * activity's state is saved.  See {@link FragmentManager#beginTransaction()
-     * FragmentManager.openTransaction()} for further discussion on this.
-     * <p/>
-     * <p>This function is guaranteed to be called prior to the release of
-     * the last data that was supplied for this Loader.  At this point
-     * you should remove all use of the old data (since it will be released
-     * soon), but should not do your own release of the data since its Loader
-     * owns it and will take care of that.  The Loader will take care of
-     * management of its data so you don't have to.  In particular:
-     * <p/>
-     * <ul>
-     * <li> <p>The Loader will monitor for changes to the data, and report
-     * them to you through new calls here.  You should not monitor the
-     * data yourself.  For example, if the data is a {@link Cursor}
-     * and you place it in a {@link CursorAdapter}, use
-     * the {@link CursorAdapter#CursorAdapter(Context,
-     * Cursor, int)} constructor <em>without</em> passing
-     * in either {@link CursorAdapter#FLAG_AUTO_REQUERY}
-     * or {@link CursorAdapter#FLAG_REGISTER_CONTENT_OBSERVER}
-     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
-     * from doing its own observing of the Cursor, which is not needed since
-     * when a change happens you will get a new Cursor throw another call
-     * here.
-     * <li> The Loader will release the data once it knows the application
-     * is no longer using it.  For example, if the data is
-     * a {@link Cursor} from a {@link CursorLoader},
-     * you should not call close() on it yourself.  If the Cursor is being placed in a
-     * {@link CursorAdapter}, you should use the
-     * {@link CursorAdapter#swapCursor(Cursor)}
-     * method so that the old Cursor is not closed.
-     * </ul>
-     *
-     * @param loader The Loader that has finished.
-     * @param data   The data generated by the Loader.
-     */
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        adapter.swapCursor(data);
-    }
-
-    /**
-     * Called when a previously created loader is being reset, and thus
-     * making its data unavailable.  The application should at this point
-     * remove any references it has to the Loader's data.
-     *
-     * @param loader The Loader that is being reset.
-     */
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        adapter.swapCursor(null);
-    }
-
-    /**
      * Returns the item BaseColumn ID (_id)
      *
      * @param v
@@ -194,4 +143,87 @@ public class MainActivityFragment extends Fragment
         }
         return new Integer(((TextView) v.findViewById(R.id.row_id)).getText().toString());
     }
+
+    private class DBLoader implements LoaderManager.LoaderCallbacks<Cursor> {
+
+        /**
+         * Instantiate and return a new Loader for the given ID.
+         *
+         * @param id   The ID whose loader is to be created.
+         * @param args Any arguments supplied by the caller.
+         * @return Return a new Loader instance that is ready to start loading.
+         */
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            return new DBCursorLoader(
+                    getActivity(),
+                    DBHelper.getInstance(getActivity()),
+                    PROJECTION,
+                    null,
+                    null,
+                    null);
+        }
+
+        /**
+         * Called when a previously created loader has finished its load.  Note
+         * that normally an application is <em>not</em> allowed to commit fragment
+         * transactions while in this call, since it can happen after an
+         * activity's state is saved.  See {@link FragmentManager#beginTransaction()
+         * FragmentManager.openTransaction()} for further discussion on this.
+         * <p/>
+         * <p>This function is guaranteed to be called prior to the release of
+         * the last data that was supplied for this Loader.  At this point
+         * you should remove all use of the old data (since it will be released
+         * soon), but should not do your own release of the data since its Loader
+         * owns it and will take care of that.  The Loader will take care of
+         * management of its data so you don't have to.  In particular:
+         * <p/>
+         * <ul>
+         * <li> <p>The Loader will monitor for changes to the data, and report
+         * them to you through new calls here.  You should not monitor the
+         * data yourself.  For example, if the data is a {@link Cursor}
+         * and you place it in a {@link CursorAdapter}, use
+         * the {@link CursorAdapter#CursorAdapter(Context,
+         * Cursor, int)} constructor <em>without</em> passing
+         * in either {@link CursorAdapter#FLAG_AUTO_REQUERY}
+         * or {@link CursorAdapter#FLAG_REGISTER_CONTENT_OBSERVER}
+         * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
+         * from doing its own observing of the Cursor, which is not needed since
+         * when a change happens you will get a new Cursor throw another call
+         * here.
+         * <li> The Loader will release the data once it knows the application
+         * is no longer using it.  For example, if the data is
+         * a {@link Cursor} from a {@link CursorLoader},
+         * you should not call close() on it yourself.  If the Cursor is being placed in a
+         * {@link CursorAdapter}, you should use the
+         * {@link CursorAdapter#swapCursor(Cursor)}
+         * method so that the old Cursor is not closed.
+         * </ul>
+         *
+         * @param loader The Loader that has finished.
+         * @param data   The data generated by the Loader.
+         */
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            adapter.swapCursor(data);
+        }
+
+        /**
+         * Called when a previously created loader is being reset, and thus
+         * making its data unavailable.  The application should at this point
+         * remove any references it has to the Loader's data.
+         *
+         * @param loader The Loader that is being reset.
+         */
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            adapter.swapCursor(null);
+        }
+
+    }
+
+    private void loadMore() {
+        adapter.getCursor().moveToLast();
+    }
+
 }
